2c9d5cc83f58feb192182abb1ccce21bdd33d5db
Winter <panwentao1301@google.com>
2022-07-02 21:13:00 +0800

    新增论文筛选功能,数据由js/papers.js导入,并添加对应的css文件

---
+++ js/papers.js
@@ -0,0 +1,32 @@
+const papers = [
+    {
+        cite: "Chen S, Guo Z, Li X, et al. Query2Set: Single-to-Multiple Partial Fingerprint Recognition Based on Attention Mechanism[J]. IEEE Transactions on Information Forensics and Security, 2022, 17: 1243-1253. ", // 论文引用描述
+        year: "2022", //日期
+        type: "J", // Conference / Journal
+        area: "CV", // RL(reinforcement learning), CV(computer vision), Others
+        note: "JCR Q1", //文章说明
+        paper: "https://ieeexplore.ieee.org/document/9733330", // 文章链接
+        code: "", // 代码链接
+        demo: "", // 展示链接
+    },
+    {
+        cite: "R Yang, Y Lu, W Li, H Sun, M Fang, Y Du, X Li, et al. Rethinking Goal-conditioned Supervised Learning and Its Connection to Offline RL[C]. In Proceedings of the International Conference on Learning Representations (ICLR-22), 2022.",
+        year: "2022",
+        type: "C", 
+        area: "RL", 
+        note: "THU-A",
+        paper: "https://openreview.net/forum?id=KJztlfGPdwW",
+        code: "https://github.com/YangRui2015/AWGCSL",
+        demo: "",
+    },
+    {
+        cite: "J Yan, H Chen, K Wang, Y Ji, Y Zhu, J Li, D Xie, Z Xu, J Huang, S Cheng, X Li, J Yao, Hierarchical attention guided framework for multi-resolution collaborative whole slide image segmentation[C]. In Proceedings of the International Conference on Medical Image Computing and Computer-Assisted Intervention (MICCAI-21), 2021: 153-163.",
+        year: "2021",
+        type: "C", 
+        area: "CV", 
+        note: "THU-A",
+        paper: "https://link.springer.com/chapter/10.1007/978-3-030-87237-3_15",
+        code: "",
+        demo: "",
+    }
+]
